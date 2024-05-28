6b54decac9ebaefa6684e3136a9f607d0516d849
braegg <brandonseggleston@gmail.com>
2024-05-28 15:10:16 -0500

    This is my first 201 code challenge

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # ops-201w11-code-challenges
 201 code challenges
+A collection of my code challenges for cyber security
---
+++ helloworld.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Script Name:                  Hello World
+# Author:                       Brandon Eggleston
+# Date of latest revision:      05/28/2024
+# Purpose:                      Prints something to the screen
+
+# Declaration of variables
+
+# Declaration of functions
+
+
+# Main
+
+echo "hello world"
+
+# End

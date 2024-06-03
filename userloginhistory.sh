26f090afa0cbcf46e23b3f5104d77bf5b40cd212
braegg <brandonseggleston@gmail.com>
2024-06-03 11:27:30 -0500

    Script prints login history of machine 3 times

---
+++ userloginhistory.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Script Name:                  User Login History
+# Author:                       Brandon Eggleston
+# Date of latest revision:      06/03/2024
+# Purpose:                      Prints login history of this machine
+
+# Declaration of variables
+
+# Declaration of functions
+# This function calls the login history of the machine
+login_history() {
+    last
+    }
+
+# Main
+
+login_history
+login_history
+login_history
+
+# End
